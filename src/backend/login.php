c4664e056ba58a1e56b8030c5312220f63802fdc
josh <jlagrimas1808@gmail.com>
2024-10-21 02:17:26 +0800

    On going: Password Hashing

--- src/backend/login.php
+++ src/backend/login.php
@@ -2,33 +2,56 @@
 session_start();
 
  //db connection
-include('connection.php');
+$conn = new mysqli('localhost', 'root', '', 'db_mis');
 
+if (isset($_POST['login'])){
+    //check for empty values
+    if(empty(trim($_POST['pass'])) && empty(trim($_POST['email']))){
+        $email = $_POST['email'];
+        $pass = $_POST['pass'];
+        
+        $options = ['cost' => 13];
+        $hash = password_hash($pass, PASSWORD_BCRYPT, $options); 
+        
+        $conn->query("INSERT INTO users (email, password) VALUES ('$email', '$hash')");
 
- if (isset($_POST['login'])){
-     $email = $_POST['email'];
-     $pass = $_POST['pass'];
+        
+        /*
+        //prepared stmt to avoid sql inject
+        $query = 'SELECT * FROM users WHERE users.email = :e AND users.password = :p';
+        $stmt = $dbh->prepare($query);
+        $stmt->execute(array(":e" => $email, ":p" => $hash));   
+        
+        
+        if ($stmt->rowCount() == 1) {
+            echo "found one user";
+            $stmt->fetch();
+            $_SESSION['users'] = $stmt->fetch();
+        }
+        
+        //check if user exists
+        if($stmt->rowCount() > 0){
+            //fetch user data
+            $_SESSION['users'] = $stmt->fetch();
+            $_SESSION['login_msg'] = "User Exists.";    
+            //redirect to this location
+            header('location: ../signup.php');
+        } else {
+            //return to page
+            $_SESSION['login_msg'] = "User does not exists.";
+            header('location: ../loginpage.php');
+        }   
+        $pdo = null;
 
-     $hash = password_hash($pass, PASSWORD_DEFAULT);
-     //prepared stmt to avoid sql inject
-     $query = 'SELECT * FROM users WHERE users.email = :e AND users.password = :p';
-     $stmt = $dbh->prepare($query);
-     $stmt->execute(array(":e" => $email, ":p" => $pass));
+    */
+    } else $error_msg = "Incorrect email or password.";
+}
+ //password hash
 
-     //check if user exists
-     if($stmt->rowCount() > 0){
-         //fetch user data
-         $_SESSON['users'] = $stmt->fetch();
-         $_SESSION['login_msg'] = "User Exists.";
-
-         //redirect to this location
-         header('location: ../loginpage.php');
-     } else {
-         //return to page
-         $_SESSION['login_msg'] = "User does not exists.";
-         header('location: ../loginpage.php');
-     }
-
-     $pdo = null;
- }
+ //$sql = "UPDATE users SET password = $hash WHERE email = $email";   
+ //password verify
+ //$data = $stmt->fetch();
+ //if (password_verify($pass, $data['pass'])) {
+ //   echo "Password verified";
+ //} else echo "Invalid password"; 
 ?>
--- src/loginpage.php
+++ src/loginpage.php
@@ -31,7 +31,7 @@
                         <p class="text-3xl font-bold break-after-auto">Sign In</p>
                         <p>Access your account.</p>
                     </div>
-                    <form action="backend/login.php" method="POST">
+                    <form action="" method="POST">
                         <div>
                             <input name="email" type="text" autocomplete="off" class="block bg-transparent w-72 border-2 border-sg text-m p-2 peer rounded-md focus:outline-none focus:ring-0 focus:border-bg-c" placeholder=" "/> 
                             <label class="absolute text-sg pointer-events-none text-sm duration-300  transform -translate-y-13.5 -translate-x-1 pl-2 pr-2 scale-75 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-8 peer-placeholder-shown:translate-x-2 peer-focus:scale-75 peer-focus:-translate-x-1 peer-focus:-translate-y-13.5 z-10 bg-c pl-1 text-left rounded-2xl ">Email Address</label>
